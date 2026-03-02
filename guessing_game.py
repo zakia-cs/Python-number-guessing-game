18724dab14948f1c8d0040dfa87d1668cb11fd53
zakia-cs <zakiamim97@gmail.com>
2026-03-02 23:55:18 +0600

    Update guessing_game.py
    Update guessing_game.py with full code

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,14 @@
+import random
 
+secret = random.randint(1, 10)
+guess = 0
+
+while guess != secret:
+    guess = int(input("guess a random number between 1 to 10: "))
+
+    if guess < secret:
+        print("try a higher number")
+    elif guess > secret:
+        print("try a lower number")
+    else:
+        print("congratulation🎉 you guessed it right")
